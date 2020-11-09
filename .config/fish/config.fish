a6821c22ee84c0b014ec95597db18575bcd16ba9
Steve Traylen <steve.traylen@cern.ch>
2020-11-09 10:10:58 +0100

    Set openstack variables in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,15 @@
+
+# Openstack
+set -gx OS_AUTH_TYPE v3fedkerb
+set -gx OS_AUTH_URL "https://keystone.cern.ch/v3"
+set -gx OS_IDENTITY_API_VERSION 3
+set -gx OS_IDENTITY_PROVIDER sssd
+set -gx OS_MUTUAL_AUTH disabled
+set -gx OS_PROJECT_DOMAIN_ID default
+set -gx OS_PROTOCOL kerberos
+set -gx OS_REGION_NAME cern
+set -gx OS_PROJECT_NAME "Personal straylen"
+
 set -g fish_greeting
 # Start ssh-agent
 fish_ssh_agent
