16dd81ff33e7182fdace00d62974427ce234f785
Steve Traylen <steve.traylen@cern.ch>
2020-11-09 12:57:57 +0100

    Set openstack in a function

---
+++ .config/fish/functions/os_env.fish
@@ -0,0 +1,19 @@
+function os_env
+  set -gx OS_AUTH_TYPE v3fedkerb
+  set -gx OS_AUTH_URL "https://keystone.cern.ch/v3"
+  set -gx OS_IDENTITY_API_VERSION 3
+  set -gx OS_IDENTITY_PROVIDER sssd
+  set -gx OS_MUTUAL_AUTH disabled
+  set -gx OS_PROJECT_DOMAIN_ID default
+  set -gx OS_PROTOCOL kerberos
+  set -gx OS_REGION_NAME cern
+
+  switch $argv
+    case 'me'
+      set -gx OS_PROJECT_NAME "Personal straylen"
+    case 'plus'
+      set -gx OS_PROJECT_NAME "IT LxPlus"
+    case '*'
+      echo 'plus , me or  ..'
+  end
+end
