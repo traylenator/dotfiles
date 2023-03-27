de42c4d793909a593d57f40c00986b534bbcf49d
Steve Traylen <steve.traylen@cern.ch>
2023-03-27 16:21:07 +0200

    Add hammercloud

--- .config/fish/functions/os_env.fish
+++ .config/fish/functions/os_env.fish
@@ -12,6 +12,9 @@ function os_env
     case 'me'
       set -gx OS_PROJECT_NAME "Personal straylen"
       set -gx OS_REGION_NAME "cern"
+    case 'hammer'
+      set -gx OS_PROJECT_NAME "IT HammerCloud"
+      set -gx OS_REGION_NAME "cern"
     case 'plus'
       set -gx OS_PROJECT_NAME "IT LxPlus"
       set -gx OS_REGION_NAME "cern"
