17273ba113a7cfba7feae5718d5afcec25d401f0
Steve Traylen <steve.traylen@cern.ch>
2023-04-25 13:11:24 +0200

    Correct myproxy command

--- .config/fish/functions/globus-setup.fish
+++ .config/fish/functions/globus-setup.fish
@@ -1,5 +1,5 @@
 function globus-setup
   openssl pkcs12 -in {$HOME}/.globus/myCertificate.p12 -clcerts -nokeys -out {$HOME}/.globus/usercert.pem
   openssl pkcs12 -in {$HOME}/.globus/myCertificate.p12 -nocerts -out  {$HOME}/.globus/userkey.pem
-  myproxy-init -s myproxy.cern.ch -l nagios -c 0 -r "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=samtf/CN=555091/CN=Robot: SAM Test Framework" -n -k NagiosRetrieve-ETF-ops
+  myproxy-init -s myproxy.cern.ch -l nagios -c 0 -x -Z "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=samtf/CN=555091/CN=Robot: SAM Test Framework" -n -k NagiosRetrieve-ETF-ops
 end
