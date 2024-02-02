327efb06fdf63660af5d2bbc5b0ac1afa1680bd3
Steve Traylen <steve.traylen@cern.ch>
2024-02-02 09:40:47 +0100

    Do not delegate as root

--- .config/fish/functions/ai-foreman.fish
+++
@@ -1,3 +0,0 @@
-function ai-foreman
-  podman run --network=host  -v  /run/.heim_org.h5l.kcm-socket:/run/.heim_org.h5l.kcm-socket -e HTTPS_PROXY=socks5://127.0.0.1:8000  -e KRB5CCNAME=KCM:  gitlab-registry.cern.ch/ai-config-team/ai-tools ai-foreman $argv
-end
