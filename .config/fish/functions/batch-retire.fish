f01c457651c031f423a179b393ab70312ea7be40
Steve Traylen <steve.traylen@cern.ch>
2024-05-27 13:58:50 +0200

    function to retire batch

--- .config/fish/functions/batch-retire.fish
+++ .config/fish/functions/batch-retire.fish
@@ -1,6 +1,10 @@
 # roger update --appstate draining --message "{\"JobDate\": $(date +%s), \"TargetDate\": $(( $(date +%s) + 3600 * 24 )), \"Reboot\": \"asap\"}" b9g03p1541.cern.ch
 #
 function batch-retire
-  #git clone https://:\@gitlab.cern.ch:8443/ai/it-puppet-"$argv".git "$argv"
-  roger update --appstate draining --message "{\"JobDate\": (date +%s), \"TargetDate\": (( math (date +%s) + (3600 * 24 ))), \"Reboot\": \"asap\"}" $argv
+  set NOW (date +%s)
+  set WHEN (math $NOW + 3600 \* 24)
+  set MESSAGE {\"JobDate\": "(date +%s)", \"TargetDate\": "(math (date +%s) + 3600 \* 24) ", \"Reboot\": \"asap\"}
+  set MESSAGE "{\"JobDate\": $NOW, \"TargetDate\": $WHEN, \"Reboot\": \"asap\"}"
+  echo $MESSAGE | jq .
+  roger update --appstate draining --message $MESSAGE $argv
 end
